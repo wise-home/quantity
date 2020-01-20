be8d5cf1a6e72275a0bd95d6885f376102c90226
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-01-20 11:23:47 +0100

    Bump 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
-# 0.1.0
+## 0.2.0
+
+* Add basic math functions, 1
+* Update README.md with instructions for basic bath functions, 5
+* Setup CI, 3
+
+## 0.1.0
 
 * Initial release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quantity.MixProject do
   def project do
     [
       app: :quantity,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
