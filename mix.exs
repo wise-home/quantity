e32c003d5f56c92ca3d743ceac1792f25e69c176
aerlingssonwh <arer@wisehome.dk>
2022-11-01 16:13:30 +0100

    Bump 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quantity.MixProject do
   def project do
     [
       app: :quantity,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
